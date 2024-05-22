57a0dd1c5024725544ce82fba9a5778a1b7bfb2a
nyagamunene <stevenyaga2014@gmail.com>
2024-05-22 19:56:00 +0300

    Corrected logging message
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -162,11 +162,20 @@ func newService(pub messaging.Publisher, tc magistrala.AuthzServiceClient, logge
 }
 
 func proxyHTTP(ctx context.Context, cfg server.Config, logger *slog.Logger, sessionHandler session.Handler) error {
-	httpConfig := mproxy.Config{
-		Address:    fmt.Sprintf("%s:%s", "", cfg.Port),
-		Target:     fmt.Sprintf("%s:%s", targetHTTPHost, targetHTTPPort),
-		PathPrefix: "/",
+	httpConfig, err := mproxy.NewConfig(env.Options{Prefix: envPrefix})
+	if err != nil {
+		return err
 	}
+	if httpConfig.Address == "" {
+		httpConfig.Address = fmt.Sprintf("%s:%s", "", cfg.Port)
+	}
+	if httpConfig.Target == "" {
+		httpConfig.Target = fmt.Sprintf("%s:%s", targetHTTPHost, targetHTTPPort)
+	}
+	if httpConfig.PathPrefix == "" {
+		httpConfig.PathPrefix = "/"
+	}
+
 	mp, err := mproxyhttp.NewProxy(httpConfig, sessionHandler, logger)
 	if err != nil {
 		return err
@@ -177,7 +186,11 @@ func proxyHTTP(ctx context.Context, cfg server.Config, logger *slog.Logger, sess
 	go func() {
 		errCh <- mp.Listen(ctx)
 	}()
-	logger.Info(fmt.Sprintf("%s service https server listening at %s:%s with TLS cert %s and key %s", svcName, cfg.Host, cfg.Port, cfg.CertFile, cfg.KeyFile))
+	if httpConfig.TLSConfig != nil {
+		logger.Info(fmt.Sprintf("%s service https server listening at %s:%s with TLS cert %s and key %s", svcName, cfg.Host, cfg.Port, cfg.CertFile, cfg.KeyFile))
+	} else {
+		logger.Info(fmt.Sprintf("%s service http server listening at %s:%s", svcName, cfg.Host, cfg.Port))
+	}
 	select {
 	case <-ctx.Done():
 		logger.Info(fmt.Sprintf("proxy HTTP shutdown at %s", httpConfig.Target))
