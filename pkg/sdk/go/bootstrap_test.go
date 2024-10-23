absmach/magistrala

Fix failing linter
Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

pkg/sdk/go/bootstrap_test.go
@@ -984,52 +984,52 @@ func TestRemoveBootstrap(t *testing.T) {
 		err      errors.SDKError
 	}{
 		{
-			desc:   "remove successfully",
+			desc:     "remove successfully",
 			domainID: domainID,
-			token:  validToken,
-			id:     thingId,
-			svcErr: nil,
-			err:    nil,
+			token:    validToken,
+			id:       thingId,
+			svcErr:   nil,
+			err:      nil,
 		},
 		{
-			desc:   "remove with invalid token",
+			desc:     "remove with invalid token",
 			domainID: domainID,
-			token:  invalidToken,
-			id:     thingId,
-			svcErr: svcerr.ErrAuthentication,
-			err:    errors.NewSDKErrorWithStatus(svcerr.ErrAuthentication, http.StatusUnauthorized),
+			token:    invalidToken,
+			id:       thingId,
+			svcErr:   svcerr.ErrAuthentication,
+			err:      errors.NewSDKErrorWithStatus(svcerr.ErrAuthentication, http.StatusUnauthorized),
 		},
 		{
-			desc:   "remove with non-existent thing Id",
+			desc:     "remove with non-existent thing Id",
 			domainID: domainID,
-			token:  validToken,
-			id:     invalid,
-			svcErr: svcerr.ErrNotFound,
-			err:    errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
+			token:    validToken,
+			id:       invalid,
+			svcErr:   svcerr.ErrNotFound,
+			err:      errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
 		},
 		{
-			desc:   "remove removed bootstrap",
+			desc:     "remove removed bootstrap",
 			domainID: domainID,
-			token:  validToken,
-			id:     thingId,
-			svcErr: svcerr.ErrNotFound,
-			err:    errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
+			token:    validToken,
+			id:       thingId,
+			svcErr:   svcerr.ErrNotFound,
+			err:      errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
 		},
 		{
-			desc:   "remove with empty token",
+			desc:     "remove with empty token",
 			domainID: domainID,
-			token:  "",
-			id:     thingId,
-			svcErr: nil,
-			err:    errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrBearerToken), http.StatusUnauthorized),
+			token:    "",
+			id:       thingId,
+			svcErr:   nil,
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrBearerToken), http.StatusUnauthorized),
 		},
 		{
-			desc:   "remove with empty id",
+			desc:     "remove with empty id",
 			domainID: domainID,
-			token:  validToken,
-			id:     "",
-			svcErr: nil,
-			err:    errors.NewSDKError(apiutil.ErrMissingID),
+			token:    validToken,
+			id:       "",
+			svcErr:   nil,
+			err:      errors.NewSDKError(apiutil.ErrMissingID),
 		},
 	}
 	for _, tc := range cases {

nyagamunene/supermq

pkg/sdk/go/bootstrap_test.go
@@ -1027,39 +1027,39 @@ func TestRemoveBootstrap(t *testing.T) {
 		err             errors.SDKError
 	}{
 		{
-			desc:   "remove successfully",
+			desc:     "remove successfully",
 			domainID: domainID,
-			token:  validToken,
-			id:     thingId,
-			svcErr: nil,
-			err:    nil,
+			token:    validToken,
+			id:       thingId,
+			svcErr:   nil,
+			err:      nil,
 		},
 		{
-			desc:            "remove with invalid token",
+			desc:              "remove with invalid token",
 			domainID: domainID,
-			token:           invalidToken,
-			id:              thingId,
-			authenticateErr: svcerr.ErrAuthentication,
-			err:             errors.NewSDKErrorWithStatus(svcerr.ErrAuthentication, http.StatusUnauthorized),
+			token:             invalidToken,
+			id:                thingId,
+			authenticateErr:   svcerr.ErrAuthentication,
+			err:               errors.NewSDKErrorWithStatus(svcerr.ErrAuthentication, http.StatusUnauthorized),
 		},
 		{
-			desc:   "remove with non-existent thing Id",
+			desc:     "remove with non-existent thing Id",
 			domainID: domainID,
-			token:  validToken,
-			id:     invalid,
-			svcErr: svcerr.ErrNotFound,
-			err:    errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
+			token:    validToken,
+			id:       invalid,
+			svcErr:   svcerr.ErrNotFound,
+			err:      errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
 		},
 		{
-			desc:   "remove removed bootstrap",
+			desc:     "remove removed bootstrap",
 			domainID: domainID,
-			token:  validToken,
-			id:     thingId,
-			svcErr: svcerr.ErrNotFound,
-			err:    errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
+			token:    validToken,
+			id:       thingId,
+			svcErr:   svcerr.ErrNotFound,
+			err:      errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
 		},
 		{
-			desc:   "remove with empty token",
+			desc:     "remove with empty token",
 			domainID: domainID,
 			token:  "",
 			id:     thingId,
@@ -1067,12 +1067,12 @@ func TestRemoveBootstrap(t *testing.T) {
 			err:    errors.NewSDKErrorWithStatus(apiutil.ErrBearerToken, http.StatusUnauthorized),
 		},
 		{
-			desc:   "remove with empty id",
+			desc:     "remove with empty id",
 			domainID: domainID,
-			token:  validToken,
-			id:     "",
-			svcErr: nil,
-			err:    errors.NewSDKError(apiutil.ErrMissingID),
+			token:    validToken,
+			id:       "",
+			svcErr:   nil,
+			err:      errors.NewSDKError(apiutil.ErrMissingID),
 		},
 	}
 	for _, tc := range cases {