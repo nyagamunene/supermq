9fe4e3af875156c9125703e8318eb2135a8755e9
nyagamunene <stevenyaga2014@gmail.com>
2024-10-31 12:02:35 +0300

    Update grpc api
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- api/grpc/channels/v1/channels.pb.go
+++ api/grpc/channels/v1/channels.pb.go
@@ -201,9 +201,11 @@ type AuthzReq struct {
 
 func (x *AuthzReq) Reset() {
 	*x = AuthzReq{}
-	mi := &file_channels_v1_channels_proto_msgTypes[4]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
+	if protoimpl.UnsafeEnabled {
+		mi := &file_channels_v1_channels_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
 }
 
 func (x *AuthzReq) String() string {
@@ -214,7 +216,7 @@ func (*AuthzReq) ProtoMessage() {}
 
 func (x *AuthzReq) ProtoReflect() protoreflect.Message {
 	mi := &file_channels_v1_channels_proto_msgTypes[4]
-	if x != nil {
+	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
 			ms.StoreMessageInfo(mi)
@@ -273,9 +275,11 @@ type AuthzRes struct {
 
 func (x *AuthzRes) Reset() {
 	*x = AuthzRes{}
-	mi := &file_channels_v1_channels_proto_msgTypes[5]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
+	if protoimpl.UnsafeEnabled {
+		mi := &file_channels_v1_channels_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
 }
 
 func (x *AuthzRes) String() string {
@@ -286,7 +290,7 @@ func (*AuthzRes) ProtoMessage() {}
 
 func (x *AuthzRes) ProtoReflect() protoreflect.Message {
 	mi := &file_channels_v1_channels_proto_msgTypes[5]
-	if x != nil {
+	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
 			ms.StoreMessageInfo(mi)
