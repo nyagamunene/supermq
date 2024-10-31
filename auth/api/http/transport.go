9b06240deab5717c3bff9a1bcb4d842cf5da647a
nyagamunene <stevenyaga2014@gmail.com>
2024-10-31 11:55:27 +0300

    Move pat to auth
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- auth/api/http/transport.go
+++ auth/api/http/transport.go
@@ -8,16 +8,20 @@ import (
 
 	"github.com/absmach/magistrala"
 	"github.com/absmach/magistrala/auth"
+	mgauthn "github.com/absmach/magistrala/pkg/authn"
 	"github.com/absmach/magistrala/auth/api/http/keys"
 	"github.com/go-chi/chi/v5"
+	"github.com/absmach/magistrala/auth/api/http/pats"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // MakeHandler returns a HTTP handler for API endpoints.
-func MakeHandler(svc auth.Service, logger *slog.Logger, instanceID string) http.Handler {
+func MakeHandler(svc auth.Service, authn mgauthn.Authentication, logger *slog.Logger, instanceID string) http.Handler {
 	mux := chi.NewRouter()
 
 	mux = keys.MakeHandler(svc, mux, logger)
+	mux = pats.MakeHandler(svc, mux, authn,logger)
+
 
 	mux.Get("/health", magistrala.Health("auth", instanceID))
 	mux.Handle("/metrics", promhttp.Handler())
