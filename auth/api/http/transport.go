4dfd4c16135890303088c1be680be81a8c7a01a3
nyagamunene <stevenyaga2014@gmail.com>
2024-10-31 13:19:56 +0300

    Update pat service
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- auth/api/http/transport.go
+++ auth/api/http/transport.go
@@ -8,20 +8,18 @@ import (
 
 	"github.com/absmach/magistrala"
 	"github.com/absmach/magistrala/auth"
-	mgauthn "github.com/absmach/magistrala/pkg/authn"
 	"github.com/absmach/magistrala/auth/api/http/keys"
-	"github.com/go-chi/chi/v5"
 	"github.com/absmach/magistrala/auth/api/http/pats"
+	"github.com/go-chi/chi/v5"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // MakeHandler returns a HTTP handler for API endpoints.
-func MakeHandler(svc auth.Service, authn mgauthn.Authentication, logger *slog.Logger, instanceID string) http.Handler {
+func MakeHandler(svc auth.Service, logger *slog.Logger, instanceID string) http.Handler {
 	mux := chi.NewRouter()
 
 	mux = keys.MakeHandler(svc, mux, logger)
-	mux = pats.MakeHandler(svc, mux, authn,logger)
-
+	mux = pats.MakeHandler(svc, mux, logger)
 
 	mux.Get("/health", magistrala.Health("auth", instanceID))
 	mux.Handle("/metrics", promhttp.Handler())
