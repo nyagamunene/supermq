311c2af8a63fafd711e00541abf57427cd6198a5
Felix Gateru <felix.gateru@gmail.com>
2024-11-11 14:31:38 +0300

    NOISSUE - Rename Things to Clients
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>
    Signed-off-by: Arvindh <arvindh91@gmail.com>

--- api/grpc/auth/v1/auth.pb.go
+++ api/grpc/auth/v1/auth.pb.go
@@ -32,9 +32,11 @@ type AuthNReq struct {
 
 func (x *AuthNReq) Reset() {
 	*x = AuthNReq{}
-	mi := &file_auth_v1_auth_proto_msgTypes[0]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
+	if protoimpl.UnsafeEnabled {
+		mi := &file_auth_v1_auth_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
 }
 
 func (x *AuthNReq) String() string {
@@ -45,7 +47,7 @@ func (*AuthNReq) ProtoMessage() {}
 
 func (x *AuthNReq) ProtoReflect() protoreflect.Message {
 	mi := &file_auth_v1_auth_proto_msgTypes[0]
-	if x != nil {
+	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
 			ms.StoreMessageInfo(mi)
@@ -78,9 +80,11 @@ type AuthNRes struct {
 
 func (x *AuthNRes) Reset() {
 	*x = AuthNRes{}
-	mi := &file_auth_v1_auth_proto_msgTypes[1]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
+	if protoimpl.UnsafeEnabled {
+		mi := &file_auth_v1_auth_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
 }
 
 func (x *AuthNRes) String() string {
@@ -91,7 +95,7 @@ func (*AuthNRes) ProtoMessage() {}
 
 func (x *AuthNRes) ProtoReflect() protoreflect.Message {
 	mi := &file_auth_v1_auth_proto_msgTypes[1]
-	if x != nil {
+	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
 			ms.StoreMessageInfo(mi)
@@ -492,6 +496,56 @@ func init() { file_auth_v1_auth_proto_init() }
 func file_auth_v1_auth_proto_init() {
 	if File_auth_v1_auth_proto != nil {
 		return
+	}
+	if !protoimpl.UnsafeEnabled {
+		file_auth_v1_auth_proto_msgTypes[0].Exporter = func(v any, i int) any {
+			switch v := v.(*AuthNReq); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_auth_v1_auth_proto_msgTypes[1].Exporter = func(v any, i int) any {
+			switch v := v.(*AuthNRes); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_auth_v1_auth_proto_msgTypes[2].Exporter = func(v any, i int) any {
+			switch v := v.(*AuthZReq); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_auth_v1_auth_proto_msgTypes[3].Exporter = func(v any, i int) any {
+			switch v := v.(*AuthZRes); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
