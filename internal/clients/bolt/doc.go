f852a12f1dcb78f565884cf0020c6accc6cd9b6c
nyagamunene <stevenyaga2014@gmail.com>
2024-12-05 16:08:51 +0300

    fix environment variables
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- internal/clients/bolt/doc.go
+++ internal/clients/bolt/doc.go
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package BoltDB contains the domain concept definitions needed to support
-// Magistrala BoltDB database functionality.
+// Supermq BoltDB database functionality.
 //
 // It provides the abstraction of the BoltDB database service, which is used
 // to configure, setup and connect to the BoltDB database.
