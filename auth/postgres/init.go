ea2e4ae0961bd528a4f05ba8c22cae5c76738bb3
nyagamunene <stevenyaga2014@gmail.com>
2025-02-03 12:46:04 +0300

    initial implemenation
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- auth/postgres/init.go
+++ auth/postgres/init.go
@@ -65,6 +65,30 @@ func Migration() *migrate.MemoryMigrationSource {
                     `,
 				},
 			},
+			{
+				Id: "auth_4",
+				Up: []string{
+					`CREATE TABLE IF NOT EXISTS pats (
+						id 					VARCHAR(36) PRIMARY KEY,
+						name        		VARCHAR(254),
+						user_id 			VARCHAR(36),
+						description			TEXT,
+						domain 				VARCHAR(36),
+						secret				TEXT,
+						issued_at			TIMESTAMP,
+						expires_at 			TIMESTAMP,
+						updated_at 			TIMESTAMP,
+						revoked 			BOOLEAN,
+						revoked_at 			TIMESTAMP,
+						entity_type 		TEXT,
+						allowed_operation	TEXT[],
+						constraints 		TEXT
+					)`,
+				},
+				Down: []string{
+					`DROP TABLE IF EXISTS pats`,
+				},
+			},
 		},
 	}
 }
