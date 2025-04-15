530533c1d84a02b05320186dafaad9461fcad0bf
nyagamunene <stevenyaga2014@gmail.com>
2025-04-15 12:41:03 +0300

    initial implementation
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- cmd/groups/main.go
+++ cmd/groups/main.go
@@ -11,6 +11,7 @@ import (
 	"log/slog"
 	"net/url"
 	"os"
+	"time"
 
 	chclient "github.com/absmach/callhome/pkg/client"
 	"github.com/absmach/supermq"
@@ -74,17 +75,25 @@ const (
 )
 
 type config struct {
-	LogLevel            string  `env:"SMQ_GROUPS_LOG_LEVEL"          envDefault:"info"`
-	InstanceID          string  `env:"SMQ_GROUPS_INSTANCE_ID"        envDefault:""`
-	JaegerURL           url.URL `env:"SMQ_JAEGER_URL"                envDefault:"http://localhost:4318/v1/traces"`
-	SendTelemetry       bool    `env:"SMQ_SEND_TELEMETRY"            envDefault:"true"`
-	ESURL               string  `env:"SMQ_ES_URL"                    envDefault:"nats://localhost:4222"`
-	ESConsumerName      string  `env:"SMQ_GROUPS_EVENT_CONSUMER"     envDefault:"groups"`
-	TraceRatio          float64 `env:"SMQ_JAEGER_TRACE_RATIO"        envDefault:"1.0"`
-	SpicedbHost         string  `env:"SMQ_SPICEDB_HOST"              envDefault:"localhost"`
-	SpicedbPort         string  `env:"SMQ_SPICEDB_PORT"              envDefault:"50051"`
-	SpicedbSchemaFile   string  `env:"SMQ_SPICEDB_SCHEMA_FILE"       envDefault:"schema.zed"`
-	SpicedbPreSharedKey string  `env:"SMQ_SPICEDB_PRE_SHARED_KEY"    envDefault:"12345678"`
+	LogLevel                   string        `env:"SMQ_GROUPS_LOG_LEVEL"          envDefault:"info"`
+	InstanceID                 string        `env:"SMQ_GROUPS_INSTANCE_ID"        envDefault:""`
+	JaegerURL                  url.URL       `env:"SMQ_JAEGER_URL"                envDefault:"http://localhost:4318/v1/traces"`
+	SendTelemetry              bool          `env:"SMQ_SEND_TELEMETRY"            envDefault:"true"`
+	ESURL                      string        `env:"SMQ_ES_URL"                    envDefault:"nats://localhost:4222"`
+	ESConsumerName             string        `env:"SMQ_GROUPS_EVENT_CONSUMER"     envDefault:"groups"`
+	TraceRatio                 float64       `env:"SMQ_JAEGER_TRACE_RATIO"        envDefault:"1.0"`
+	SpicedbHost                string        `env:"SMQ_SPICEDB_HOST"              envDefault:"localhost"`
+	SpicedbPort                string        `env:"SMQ_SPICEDB_PORT"              envDefault:"50051"`
+	SpicedbSchemaFile          string        `env:"SMQ_SPICEDB_SCHEMA_FILE"       envDefault:"schema.zed"`
+	SpicedbPreSharedKey        string        `env:"SMQ_SPICEDB_PRE_SHARED_KEY"    envDefault:"12345678"`
+	AuthCalloutURLs            []string      `env:"SMQ_AUTH_CALLOUT_URLS"             envDefault:"" envSeparator:","`
+	AuthCalloutMethod          string        `env:"SMQ_AUTH_CALLOUT_METHOD"           envDefault:"POST"`
+	AuthCalloutTLSVerification bool          `env:"SMQ_AUTH_CALLOUT_TLS_VERIFICATION" envDefault:"true"`
+	AuthCalloutTimeout         time.Duration `env:"SMQ_AUTH_CALLOUT_TIMEOUT"          envDefault:"10s"`
+	AuthCalloutCACert          string        `env:"SMQ_AUTH_CALLOUT_CA_CERT"          envDefault:""`
+	AuthCalloutCert            string        `env:"SMQ_AUTH_CALLOUT_CERT"             envDefault:""`
+	AuthCalloutKey             string        `env:"SMQ_AUTH_CALLOUT_KEY"              envDefault:""`
+	AuthCalloutPermissions     []string      `env:"SMQ_AUTH_CALLOUT_INVOKE_PERMISSIONS" envDefault:"" envSeparator:","`
 }
 
 func main() {
@@ -175,7 +184,14 @@ func main() {
 	}
 	defer domainsHandler.Close()
 
-	authz, authzHandler, err := authsvcAuthz.NewAuthorization(ctx, authClientConfig, domAuthz)
+	client, err := authsvcAuthz.LoadCerts(cfg.AuthCalloutTLSVerification, cfg.AuthCalloutCACert, cfg.AuthCalloutKey, cfg.AuthCalloutCACert, cfg.AuthCalloutTimeout)
+	if err != nil {
+		logger.Error(err.Error())
+		exitCode = 1
+		return
+	}
+
+	authz, authzHandler, err := authsvcAuthz.NewAuthorization(ctx, authClientConfig, domAuthz, client, cfg.AuthCalloutMethod, cfg.AuthCalloutURLs, cfg.AuthCalloutPermissions)
 	if err != nil {
 		logger.Error("failed to create authz " + err.Error())
 		exitCode = 1
