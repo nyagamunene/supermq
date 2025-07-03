a07881d7b281fbe71262e919b5b2e4b5c88781a4
nyagamunene <stevenyaga2014@gmail.com>
2025-07-03 16:24:27 +0300

    address comments
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- users/api/endpoints.go
+++ users/api/endpoints.go
@@ -113,14 +113,13 @@ func listUsersEndpoint(svc users.Service) endpoint.Endpoint {
 
 		res := usersPageRes{
 			pageRes: pageRes{
-				Total: page.Total,
+				Offset: page.Offset,
+				Limit:  page.Limit,
+				Total:  page.Total,
 			},
 			Users: []viewUserRes{},
 		}
-		if !req.onlyTotal {
-			res.Offset = page.Offset
-			res.Limit = page.Limit
-		}
+
 		for _, user := range page.Users {
 			res.Users = append(res.Users, viewUserRes{User: user})
 		}
