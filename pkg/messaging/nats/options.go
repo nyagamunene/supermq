c3c11a92edd9ceff203a633d7def4a8d3b4fccc5
nyagamunene <stevenyaga2014@gmail.com>
2025-08-21 13:32:10 +0300

    address comments
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- pkg/messaging/nats/options.go
+++ pkg/messaging/nats/options.go
@@ -14,63 +14,64 @@ import (
 var (
 	// ErrInvalidType is returned when the provided value is not of the expected type.
 	ErrInvalidType = errors.New("invalid type")
+)
 
-	jsStreamConfig = jetstream.StreamConfig{
-		Name:              "m",
-		Description:       "SuperMQ stream for sending and receiving messages in between SuperMQ channels",
-		Subjects:          []string{"m.>"},
-		Retention:         jetstream.LimitsPolicy,
-		MaxMsgsPerSubject: 1e6,
-		MaxAge:            time.Hour * 24,
-		MaxMsgSize:        1024 * 1024,
-		Discard:           jetstream.DiscardOld,
-		Storage:           jetstream.FileStorage,
+// SMQJetStreamConfig extends jetstream.StreamConfig with slow consumer monitoring settings
+type SMQJetStreamConfig struct {
+	jetstream.StreamConfig
+	
+	SlowConsumer SlowConsumerConfig `json:"slow_consumer,omitempty"`
+}
+
+type SlowConsumerConfig struct {
+	// MaxPendingBytes maps to JetStream ConsumerConfig.MaxRequestMaxBytes
+	// Controls the maximum bytes per batch request (closest JetStream equivalent)
+	MaxPendingBytes int `json:"max_pending_bytes,omitempty"`
+
+	// EnableDroppedMsgTracking enables logging of message redeliveries
+	// which can indicate slow consumer behavior in JetStream
+	EnableDroppedMsgTracking bool `json:"enable_dropped_msg_tracking,omitempty"`
+}
+
+var (
+	defaultJetStreamConfig = SMQJetStreamConfig{
+		StreamConfig: jetstream.StreamConfig{
+			Name:              "m",
+			Description:       "SuperMQ stream for sending and receiving messages in between SuperMQ channels",
+			Subjects:          []string{"m.>"},
+			Retention:         jetstream.LimitsPolicy,
+			MaxMsgsPerSubject: 1e6,
+			MaxAge:            time.Hour * 24,
+			MaxMsgSize:        1024 * 1024,
+			Discard:           jetstream.DiscardOld,
+			Storage:           jetstream.FileStorage,
+			ConsumerLimits: jetstream.StreamConsumerLimits{
+				MaxAckPending: defaultMaxPendingMsgs,
+			},
+		},
+		SlowConsumer: SlowConsumerConfig{
+			MaxPendingBytes:          defaultMaxPendingBytes,
+			EnableDroppedMsgTracking: defaultEnableDroppedMsgTracking,
+		},
 	}
 )
 
 const (
-	msgPrefix                          = "m"
-	defaultMaxPendingMsgs              = 1000
-	defaultMaxPendingBytes             = 5 * 1024 * 1024
-	defaultEnableDroppedMsgTracking    = true
-	defaultEnableSlowConsumerDetection = true
+	msgPrefix                       = "m"
+	defaultMaxPendingMsgs           = 1000
+	defaultMaxPendingBytes          = 5 * 1024 * 1024
+	defaultEnableDroppedMsgTracking = true
 )
 
 type options struct {
 	prefix             string
-	jsStreamConfig     jetstream.StreamConfig
-	slowConsumerConfig *SlowConsumerConfig
-}
-
-type SlowConsumerConfig struct {
-	// MaxPendingMsgs maps to JetStream ConsumerConfig.MaxAckPending
-	// Controls the maximum number of outstanding unacknowledged messages
-	// Also used for slow consumer detection at 70% of this value
-	MaxPendingMsgs int
-
-	// MaxPendingBytes maps to JetStream ConsumerConfig.MaxRequestMaxBytes
-	// Controls the maximum bytes per batch request (closest JetStream equivalent)
-	MaxPendingBytes int
-
-	// EnableDroppedMsgTracking enables logging of message redeliveries
-	// which can indicate slow consumer behavior in JetStream
-	EnableDroppedMsgTracking bool
-
-	// EnableSlowConsumerDetection enables automatic slow consumer detection
-	// and logging when consumer lag exceeds 70% of MaxPendingMsgs
-	EnableSlowConsumerDetection bool
+	jsStreamConfig     SMQJetStreamConfig
 }
 
 func defaultOptions() options {
 	return options{
 		prefix:         msgPrefix,
-		jsStreamConfig: jsStreamConfig,
-		slowConsumerConfig: &SlowConsumerConfig{
-			MaxPendingMsgs:              defaultMaxPendingMsgs,
-			MaxPendingBytes:             defaultMaxPendingBytes,
-			EnableDroppedMsgTracking:    defaultEnableDroppedMsgTracking,
-			EnableSlowConsumerDetection: defaultEnableSlowConsumerDetection,
-		},
+		jsStreamConfig: defaultJetStreamConfig,
 	}
 }
 
@@ -90,8 +91,8 @@ func Prefix(prefix string) messaging.Option {
 	}
 }
 
-// JSStreamConfig sets the JetStream for the publisher or subscriber.
-func JSStreamConfig(jsStreamConfig jetstream.StreamConfig) messaging.Option {
+// JSStreamConfig sets the JetStream configuration for the publisher or subscriber.
+func JSStreamConfig(jsStreamConfig SMQJetStreamConfig) messaging.Option {
 	return func(val any) error {
 		switch v := val.(type) {
 		case *publisher:
@@ -106,14 +107,30 @@ func JSStreamConfig(jsStreamConfig jetstream.StreamConfig) messaging.Option {
 	}
 }
 
-// WithSlowConsumerConfig sets the slow consumer configuration.
+// WithSlowConsumerConfig sets the slow consumer configuration within the JetStream config.
 func WithSlowConsumerConfig(config SlowConsumerConfig) messaging.Option {
 	return func(val interface{}) error {
 		switch v := val.(type) {
 		case *publisher:
-			v.slowConsumerConfig = &config
+			v.jsStreamConfig.SlowConsumer = config
 		case *pubsub:
-			v.slowConsumerConfig = &config
+			v.jsStreamConfig.SlowConsumer = config
+		default:
+			return ErrInvalidType
+		}
+
+		return nil
+	}
+}
+
+// WithConsumerLimits sets the built-in JetStream consumer limits (MaxAckPending, etc.).
+func WithConsumerLimits(limits jetstream.StreamConsumerLimits) messaging.Option {
+	return func(val interface{}) error {
+		switch v := val.(type) {
+		case *publisher:
+			v.jsStreamConfig.ConsumerLimits = limits
+		case *pubsub:
+			v.jsStreamConfig.ConsumerLimits = limits
 		default:
 			return ErrInvalidType
 		}
