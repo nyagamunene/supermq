270dddfbb9150f48feaeedaa857477d6dec2dd7d
nyagamunene <stevenyaga2014@gmail.com>
2025-09-02 02:19:09 +0300

    update docs
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- domains/api/http/endpoint_test.go
+++ domains/api/http/endpoint_test.go
@@ -1534,7 +1534,7 @@ func TestDeleteInvitation(t *testing.T) {
 			}
 			authnCall := auth.On("Authenticate", mock.Anything, tc.token).Return(tc.session, tc.authnErr)
 			repoCall := svc.On("DeleteInvitation", mock.Anything, tc.session, tc.userID, tc.domainID).Return(tc.svcErr)
-			
+
 			data := fmt.Sprintf(`{"user_id": "%s"}`, tc.userID)
 			req := testRequest{
 				client:      is.Client(),
